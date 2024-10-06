6e661be8cd1c8cc4fb04e3b5c3c729a03c0ef3c9
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-10-07 01:11:56 +0300

    Add username and message min and max lengths

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,3 +2,9 @@ import os
 
 DB_HOST = os.getenv("DB_HOST", "mongodb://mongodb:27017")
 DB_NAME = os.getenv("DB_NAME", "ApplicationDatabase")
+
+USERNAME_MIN_LENGTH = os.getenv("USERNAME_MIN_LENGTH", 3)
+USERNAME_MAX_LENGTH = os.getenv("USERNAME_MAX_LENGTH", 30)
+
+MESSAGE_MIN_LENGTH = os.getenv("MESSAGE_MIN_LENGTH", 1)
+MESSAGE_MAX_LENGTH = os.getenv("MESSAGE_MAX_LENGTH", 400)
