7b6daf06ed1e1bd08147716c086c812688bce8b1
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-10-07 01:26:20 +0300

    Add schema with constraints to auth service (#8)

--- backend/src/services/auth/routes.py
+++ backend/src/services/auth/routes.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter, Response
 
+from services.auth.schemas import UsernameDTO
 from services.auth.service import AuthService
 
 router = APIRouter()
 
 
 @router.post("/register", status_code=201)
-async def register(username: str):
-    await AuthService.register(username)
+async def register(data: UsernameDTO):
+    await AuthService.register(data.username)
 
     return Response(status_code=201)
 
 
 @router.post("/login", status_code=200)
-async def login(username: str):
-    await AuthService.login(username)
+async def login(data: UsernameDTO):
+    await AuthService.login(data.username)
 
     return Response(status_code=200)
