057b1d72027897b89bd9954c8003e0eebcc335b3
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-10-07 00:43:32 +0300

    Add exception handlers to applications

--- backend/src/services/like/app.py
+++ backend/src/services/like/app.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from common.exceptions.exception import (
+    ApplicationException,
+    application_exception_handler,
+)
 from common.lifespan import lifespan
 from services.like.config import SERVICE_ROOT_PATH
 from services.like.routes import router
@@ -7,3 +11,4 @@ from services.like.routes import router
 app = FastAPI(root_path=SERVICE_ROOT_PATH, lifespan=lifespan)
 
 app.include_router(router)
+app.add_exception_handler(ApplicationException, application_exception_handler)
