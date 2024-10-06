9f9591ca07a910b88688d6f8b52c2ce83918dee9
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-10-07 01:22:57 +0300

    Change User Does Not Found exception text

--- backend/src/common/exceptions/user.py
+++ backend/src/common/exceptions/user.py
@@ -6,6 +6,6 @@ class UserAlreadyExists(ApplicationException):
         super().__init__(message="User already exists.", status_code=400)
 
 
-class UserDoesNotFound(ApplicationException):
+class UserDoesNotExist(ApplicationException):
     def __init__(self):
-        super().__init__(message="User does not found.", status_code=404)
+        super().__init__(message="User does not exist.", status_code=404)
--- backend/src/services/auth/service.py
+++ backend/src/services/auth/service.py
@@ -1,5 +1,5 @@
 from common.database.repositories.user import UserRepository
-from common.exceptions.user import UserAlreadyExists, UserDoesNotFound
+from common.exceptions.user import UserAlreadyExists, UserDoesNotExist
 
 
 class AuthService:
@@ -13,4 +13,4 @@ class AuthService:
     @staticmethod
     async def login(username: str) -> None:
         if not await UserRepository.is_exists(username):
-            raise UserDoesNotFound()
+            raise UserDoesNotExist()
