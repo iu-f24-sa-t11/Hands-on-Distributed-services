3c5b1777d7ffa52e69e58792f5d92a5455a201c9
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-10-07 01:23:11 +0300

    Change User Does Not Found exception text (#7)

--- backend/src/services/message/service.py
+++ backend/src/services/message/service.py
@@ -1,12 +1,12 @@
 from common.database.repositories.message import MessageRepository
 from common.database.repositories.user import UserRepository
-from common.exceptions.user import UserDoesNotFound
+from common.exceptions.user import UserDoesNotExist
 
 
 class MessageService:
     @staticmethod
     async def create(content: str, author_username: str) -> None:
         if not await UserRepository.is_exists(username=author_username):
-            raise UserDoesNotFound()
+            raise UserDoesNotExist()
 
         await MessageRepository.create(content=content, author_username=author_username)
