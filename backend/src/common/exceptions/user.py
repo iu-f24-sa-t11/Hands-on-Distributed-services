ea1d2be4b6783aa42aeaa6f89a29838135ff0fd9
Azamat Bayramov <bayramov.azamat04@gmail.com>
2024-10-07 11:18:35 +0300

    Merge pull request #12
    * Improve user exceptions
    
    * Delete unnecessary module
    
    * Improve code
    
    * Improve imports

--- backend/src/common/exceptions/user.py
+++ backend/src/common/exceptions/user.py
@@ -2,10 +2,14 @@ from common.exceptions.exception import ApplicationException
 
 
 class UserAlreadyExists(ApplicationException):
-    def __init__(self):
-        super().__init__(message="User already exists.", status_code=400)
+    def __init__(self, username: str):
+        super().__init__(
+            message=f"User with username {username} already exists.", status_code=400
+        )
 
 
 class UserDoesNotExist(ApplicationException):
-    def __init__(self):
-        super().__init__(message="User does not exist.", status_code=404)
+    def __init__(self, username: str):
+        super().__init__(
+            message=f"User with username {username} does not exist.", status_code=404
+        )
