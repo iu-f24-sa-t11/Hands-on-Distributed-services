de36a2409556e1669266f6a524838e4d6aafa4ae
MattWay224 <112853687+MattWay224@users.noreply.github.com>
2024-10-10 20:15:26 +0300

    remove caddy req

--- requiements.sh
+++ requiements.sh
@@ -11,10 +11,4 @@ sudo apt install -y docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install -y caddy
-
 echo "Installation complete!"
